24ca6202dc576e4ced06a953b0fc92ccccd603fe
Arnaud Rinquin <arnaud.rinquin@wopata.com>
2014-02-18 16:08:50 +0100

    Initial files push

---
+++ HISTORY.md
@@ -0,0 +1,4 @@
+# History
+
+* v0.0.0 Feb 20, 2014
+	* Initial port from http://jsfiddle.net/ArnaudRinquin/9t93L/
---
+++ dist/jquery.active-nav-toggler.js
@@ -0,0 +1,55 @@
+/*
+ *  jQuery Active nav toggler - v0.0.0
+ *  Toggle active class on navigation components
+ *  https://github.com/ArnaudRinquin/jquery-active-nav-toggler
+ *
+ *  Made by Arnaud Rinquin
+ *  Under MIT License
+ */
+;(function ( $, window, document, undefined ) {
+
+  var pluginName = "activeNavToggler",
+    defaults = {
+    action: "enable",
+    activeClass: "active",
+    childSelector: "li"
+  };
+
+  function Plugin ( element, options ) {
+    this.element = element;
+    this.options = $.extend( {}, defaults, options );
+    this._defaults = defaults;
+    this._name = pluginName;
+    this.init();
+  }
+
+  Plugin.prototype = {
+    init: function () {
+      if (this.options.action === "disable") {
+        this.disable(this.element);
+      } else {
+        this.enable(this.element, this.options);
+      }
+    },
+    enable: function (container, options) {
+      var $container = $(container),
+          currentlyActiveSelector = options.childSelector + "." + options.activeClass;
+
+      // Delegate click event handler
+      $container.on("click.activeTabs", options.childSelector, function () {
+        $container.find(currentlyActiveSelector).removeClass(options.activeClass);
+        $(this).addClass(options.activeClass);
+      });
+    },
+    disable: function (container) {
+      return $(container).off("click.activeTabs");
+    }
+  };
+
+  $.fn[ pluginName ] = function ( options ) {
+    return this.each(function() {
+      new Plugin( this, options );
+    });
+  };
+
+})( jQuery, window, document );
